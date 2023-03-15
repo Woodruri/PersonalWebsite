68440d4c12d7243e6f295cc39a66097d15d5fbec
Riley <woodruff.l.riley@gmail.com>
2023-03-14 19:08:32 -0700

    Added a fun little secret

--- website.js
+++ website.js
@@ -13,10 +13,18 @@ function secret() {
   let btn = document.getElementById("uhoh");
   let vert = Math.floor(Math.random() * winW);
   let horiz = Math.floor(Math.random() * winH);
+  let rot = Math.floor(Math.random() * 360);
+
+  document.body.style.backgroundImage = "url('images/elmo.gif')";
+  var imgs = document.getElementsByTagName("img");
+  for (var i = 0, l = imgs.length; i < l; i++) {
+    imgs[i].src = "images/elmo.gif";
+  }
 
   btn.style.position = "fixed";
   btn.style.left = `${vert}px`;
   btn.style.top = `${horiz}px`;
+  btn.rotate(`${rot}`);
 }
 
 function playsound() {
